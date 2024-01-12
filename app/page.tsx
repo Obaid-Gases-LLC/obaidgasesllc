d2dd3317b0dd3e37807a9137bd0b933268e59a8e
Rahul-Lap <rahulbushi69@gmail.com>
2024-01-12 14:57:12 +0530

    Update + Upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
+import { Button } from '@components/ui/button'
+import industry from '@public/industry.jpg'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
     return (
         <main className="flex flex-col">
-            <div className='h-[220px] flex justify-center items-left flex-col p-4'>
-                <p className='text-2xl'>Since 2018 </p>
-                <span className='text-4xl'>Best Services & <span>Prices</span></span>
-                <p>We are pioneers in the direct marketing since 2018 and we are very much</p>
-                <p> aware about the potentiality of its distribution in this country.</p>
+            <div className='flex justify-center relative w-full sm:h-[750px] h-[200px]'>
+                <div className='absolute text-center z-[100] font-bold'>
+                    <p className='sm:text-2xl mt-5'>Since 2018 </p>
+                    <span className='sm:text-4xl'>Best Services & <span>Prices</span></span>
+                    <p>We are pioneers in the direct marketing since 2018 and we are very much</p>
+                    <p> aware about the potentiality of its distribution in this country.</p>
+                </div>
+                <div className='absolute z-[10] w-full flex justify-center items-center'>
+                    <Image src={industry} height={1080} alt='Obaid Gases LLC' />
+                </div>  
             </div>
             <div className='sm:p-5 mt-10'>
                 <div>
@@ -18,8 +26,12 @@ export default function Home() {
                             <div>
                                 <p className='py-3'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Obaid Gases LLC is a leading force in the industrial gases sector, specializing in top-tier air products and cutting-edge equipment. Our commitment to excellence is evident in the quality and purity of our diverse range of industrial gases, including oxygen, nitrogen, argon, and helium. With state-of-the-art facilities adhering to international standards, we prioritize stringent quality control and safety measures to surpass customer expectations.</p>
                                 <p className='py-3'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Our comprehensive portfolio extends beyond gases to encompass advanced equipment like pressure vessels, cylinders, and regulators. Crafted with precision and durability, these components enhance the safe and efficient handling of industrial gases in various applications.</p>
-                                <p className='py-3'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notably, Obaid Gases LLC focuses exclusively on air products and associated equipment, excluding LPG (Liquefied Petroleum Gas) from our offerings. This specialization allows us to excel in our core areas, ensuring unparalleled expertise and reliability for our customers.</p>
-                                <p className='py-3'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;As we navigate the dynamic industrial gases landscape, Obaid Gases LLC remains dedicated to innovation, setting new benchmarks in quality and customer satisfaction. Join us as we pave the way for a future where industrial processes thrive on the excellence of Obaid Gases.</p>
+                                <p className='py-3'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We are pioneers in the direct marketing since 2018 and we are very much aware about the potentiality of its distribution in this country.........</p>
+                                <p className='py-3 flex items-center justify-center'>
+                                    <Button>
+                                        <Link href='/about/obaidgasesllc'>Read More</Link>
+                                    </Button>
+                                </p>
                             </div>
                         </div>
                         <div className='sm:w-1/2 w-full p-4'>
